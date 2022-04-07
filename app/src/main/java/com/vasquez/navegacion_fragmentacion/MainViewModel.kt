5aa83106cfeb8760bf49a33c1a9efa5b6c495c38
ErickVasquez11 <00065520@uca.edu.sv>
2022-04-07 12:41:00 -0600

    Form Modify

--- app/src/main/java/com/vasquez/navegacion_fragmentacion/MainViewModel.kt
+++ app/src/main/java/com/vasquez/navegacion_fragmentacion/MainViewModel.kt
@@ -8,6 +8,8 @@ class MainViewModel: ViewModel(){
     // Declarar 2 valores , lo que queremos que ingrese el usuario, el nombre lo guardaremos en HomeFragment y el LastName en SecondFragment
     var name = MutableLiveData("")
     var lastName = MutableLiveData("")
+    var university = MutableLiveData("")
+
 
 
 }
